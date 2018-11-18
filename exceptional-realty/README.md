a8987c96b638789e3b381d34958586c8a948ac5b
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-17 22:50:23 -0500

    first commit

---
+++ exceptional-realty/README.md
@@ -0,0 +1,10 @@
+EXCEPTIONAL REALTY GROUP WEBSITE
+---
+
+# DESCRIPTION
+
+This is an example site for the Intro to Front-end Web Development Course at The Flatiron School.
+
+# Contact
+
+email: jon@flatironschool.com
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Jonathan Grover - Artist</title>
+  <META NAME="description" content="Jonathan Grover - The artwork and vitae of Jonathan Grover.">
+	<META NAME="keywords" content="jon, jonathan, grover, john, design, web, graphic, web design, portfolio, work, artist, illustration, photography, multimedia, branding, identity, logos, artwork, teaching, teacher, San Francisco, based, sfai, san francisco art institute, interactive, media, technology">
+	<link rel="shortcut icon" href="/favicon.ico">
+  <link href="css/lightbox.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/main.css">
+	<meta name="Owner" content="Jonathan Grover">
+	<meta name="robots" content="index,follow">
+	<meta name="Googlebot" content="index,follow">
+  <meta name="Googlebot-Image" content="index,follow">
+  <meta name="verify-v1" content="hKeqO67Ge/cDKmFu1RTliKhwKc3PzDPHmedoqgpmVgQ=">
+</head>
+<body>
+<nav>
+	<h1>Jonathan Grover</h1>
+	<!-- <a class="babyblue" href="?p=about">About</a> -->
+	<a href="#" id="nest1trigger" class="babyblue">Artwork</a>
+	<div id="nest1" class="nest">
+		<a href="?p=turquoise-wind" class="graysky">Turquoise Wind</a>
+		<a href="?p=chakra-synth" class="graysky">Chakra Synth</a>
+		<a href="?p=bridge" class="graysky">Bridge</a>
+		<a href="?p=chimes" class="graysky">Chimes</a>
+		<a href="?p=shadow" class="graysky">Shadow</a>
+		<a href="?p=clotheslines" class="graysky">Clotheslines</a>
+		<a href="?p=under-tables" class="graysky">Under Tables</a>
+		<a href="?p=red-push-pull" class="graysky">Red Push Pull</a>
+		<a href="?p=twilight-maps" class="graysky">Twilight Maps</a>
+		<a href="?p=pulse" class="graysky">Pulse</a>
+		<a href="?p=ants" class="graysky">Ants</a>
+	</div><!-- nest1 -->
+	<a class="babyblue" href="?p=bio">Biography</a>
+	<a class="babyblue" href="?p=contact">Contact</a>
+	<a class="babyblue" href="?p=exhibitions">Exhibitions</a>
+	<a class="babyblue" href="?p=press">Press</a>
+	<a class="babyblue" href="?p=resources">Resources</a>
+	<a class="babyblue" href="?p=resume">Résumé</a>
+	<a class="babyblue" href="?p=statement">Statement</a>
+	<a class="raspberry" href="https://codepajamas.com" target="_blank">Teaching</a>
+</nav>
+<main>
+  <div id="txt">
+    <h5>About</h5>
+    Jonathan Grover is a new media artist that creates interactive sculptures that translate touch and motion into sound, video, and light. To learn more, please explore his artwork, biography, statement, and current resume.
+  </div>
+</main>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/js.cookie.min.js"></script>
+  <script src="js/imagesloaded.pkgd.min.js"></script>
+  <script src="js/masonry.pkgd.min.js"></script>
+  <script src="js/lightbox.min.js"></script>
+  <script src="js/main.js"></script>
+  <!-- start google analytics -->
+  <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">
+    try {
+      var pageTracker = _gat._getTracker("UA-3839866-1");
+      pageTracker._trackPageview();
+    } catch(err) {}
+  </script>
+  <!-- end google analytics -->
+</body>
+</html>
